9f1303c231251265522b11d5cda216a6244d6344
Dalja <juanbenitez2908@gmail.com>
2017-10-04 08:41:04 -0500

    Subiendo

---
+++ AhorcadoHTTP/src/java/controladores/ServletJuego.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controladores;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import modelo.LogicaJuego;
+
+/**
+ *
+ * @author Juan Benitez
+ */
+public class ServletJuego extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        
+               LogicaJuego palabra = new LogicaJuego();
+               String word = palabra.Random();
+               //System.out.println(word);
+               //String json = new Gson().toJson(word);
+               //System.out.println(json);
+               //response.setContentType("aplication/json");
+               response.setCharacterEncoding("UTF-8");
+               Writer writer = null;
+               try{
+                   writer = response.getWriter();
+                   writer.write(word);
+               }finally{
+                   try{
+                       writer.close();
+                   }catch(IOException e){
+                        System.out.println(e.getMessage());
+                   }
+               }
+               //request.getSession().setAttribute("palabra", word);
+          try {      
+        } catch (Exception error) {
+              System.out.println(error.getMessage());
+        } finally {
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
